c12efb9808c9fb22892ed9b97d509cf5c31362e5
Andrea Ortez <andreaortez11@unitec.edu>
2023-03-03 14:07:48 -0600

    arreglos

--- src/lab6p2_andreaortez/Cliente.java
+++ src/lab6p2_andreaortez/Cliente.java
@@ -3,13 +3,21 @@ package lab6p2_andreaortez;
 
 import java.util.ArrayList;
 
-public class Cliente {
+public class Cliente extends Usuario{
     ArrayList <Cancion> cancionesf = new ArrayList();
     ArrayList <Lista> lista = new ArrayList();
     ArrayList <Lista> like = new ArrayList();
 
     public Cliente() {
     }
-    
+
+    public Cliente(String user, String contra, int edad) {
+        super(user, contra, edad);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "cancionesf=" + cancionesf + ", lista=" + lista + ", like=" + like + '}';
+    }
     
 }
